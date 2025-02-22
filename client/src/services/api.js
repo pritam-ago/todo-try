06c983a1eebb4532714c34eee6af4e9c0f37a15f
Vikas Pritam <87309275+pritam-ago@users.noreply.github.com>
2025-02-22 19:37:15 +0530

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://todo-blockchain-lovat.vercel.app',  
+  baseURL: 'https://todo-try.vercel.app',  
   headers: {
     'Authorization': `Bearer ${localStorage.getItem('token')}`,
   },
